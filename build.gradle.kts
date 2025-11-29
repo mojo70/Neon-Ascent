5d5a820bdc5991a6a353ca7497b21433d3b5db49
Mojo <mojo_70@hotmail.com>
2025-11-29 16:01:43 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,10 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("androidx.room") version "2.6.1" apply false
 }
+    // Neon-Ascent/build.gradle.kts   ← PROJECT LEVEL
+    plugins {
+        // ... other plugins like 
+    }
+    
