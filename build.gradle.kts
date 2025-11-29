fc70aa430a7380d458a8f0dedc14014d5b35831a
Mojo <mojo_70@hotmail.com>
2025-11-29 15:18:27 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
-// Neon-Ascent/build.gradle.kts   ← PROJECT LEVEL
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
     id("androidx.room") version "2.6.1" apply false
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDirectory)
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
