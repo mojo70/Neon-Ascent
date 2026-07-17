84aae6a6e230178d43abc6eab452882714fa8368
joeevans <mojo_70@hotmail.com>
2026-07-17 19:30:48 -0400

    Upgrade AGP to 9.3.0 and enable parallel Gradle sync.
    Signed-off-by: joeevans <mojo_70@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build.gradle.kts
 plugins {
-    id("com.android.application") version "9.2.1" apply false
-    id("com.android.library") version "9.2.1" apply false
+    id("com.android.application") version "9.3.0" apply false
+    id("com.android.library") version "9.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("com.google.devtools.ksp") version "2.3.7" apply false
